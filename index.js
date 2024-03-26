f6794d80606aeb50593c3bf29396ddf3a493a2ed
Trevor Benoit <Trevorb042@gmail.com>
2024-03-26 15:44:43 -0600

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42; 
+    const distance = Math.abs(pickupLocation - hqLocation); 
+    return distance; 
+  }
+
+
+function distanceFromHqInFeet(pickupLocation){
+    const blocks = distanceFromHqInBlocks(pickupLocation)
+    const feetPerBlock = 264
+    return blocks * feetPerBlock
+}
+function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(start - destination);
+    const feetPerBlock = 264;
+    return blocksTravelled * feetPerBlock;
+}
+function calculatesFarePrice(start, destination){
+const distanceInFeet = distanceTravelledInFeet(start, destination);
+  
+  if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
